4230d148714346390565d3aae4b83e2f65fdf713
RuslanSalimov <mail-spam2012@yandex.ru>
2021-02-04 21:22:11 +0500

    Обязательное задание выполнено

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,90 @@
-//Создать массив arr = []
-//Записать в него 7 любых многозначных чисел в виде строк
-let arr = ['654864', '265978', '6595884', '45668789', '6546465','2695959', '65464'];
-//Вывести в консоль только те, что начинаются с цифры 2 или 4 (Должны присутствовать в массиве)
-arr.forEach(item => {
-   if(item[0] == 2 ||  +item[0] == 4) {
-       console.log(item);
-   }
-});
+//в start попадает функция, в замыкании 77
+let start = game();
+//запускаем игру
+start();
 
-
-/*
-    Вывести в столбик все простые числа от 1 до 100 (сделать при помощи цикла)
-    Рядом с каждым числом написать оба делителя данного числа
-    Например: “Делители этого числа: 1 и n”
-*/
-
-let maxCount = 10000;
-
-for(let i = 1; i <= maxCount; i++){
-    if(isSimpleNumber(i)){
-        console.log(`${i} - Делители этого числа 1 и ${i}`);
+/**
+ * Функция игры с замыканием
+ * @param targetNumber
+ * @returns {function}
+ */
+function game(targetNumber) {
+    if (targetNumber < 0 || targetNumber > 100 || !targetNumber){
+        targetNumber = getRandomInRange(0, 100);//если передали не правильное число, рандомим его
     }
+    console.log(targetNumber);
+    let message2 = ''; //дополнительное сообщение о величине числа(больше - меньше)
+    const getNumber = function (message) {
+        //если сообщение пользователю пустое, то ставим умолчание
+        if(!message){
+            message = 'Input the Number from 0 to 100';
+        }
+        let userNumber = getNumberFromUser(message2 + `\n` + message);
+        if(userNumber == null){         //если нажата Отмена
+            alert("Good bye my friend");
+            return 0;
+        } else if(userNumber == targetNumber) {         //если число угадано
+            alert("You Win!!!");
+            return 0;
+        } else if (userNumber > targetNumber) {
+            message2 = `You number is so big! `;
+        } else if (userNumber < targetNumber) {
+            message2 = `You number is so small!`;
+        }
+        getNumber(message);
+    }
+    return getNumber;
 }
 
 /**
- * Функция проверки числа на простоту
- * @param number - проверяемое число, типа number больше нуля
- * @returns {boolean} - true or false
+ * Функция получает от пользователя число, переделал на рекурсив
+ * @param message
+ * @param defaultValue
+ * @returns {number}
  */
-function isSimpleNumber(number) {
-    //проверим, что передано число
-    if(typeof number != 'number'){
-        return false;
+function getNumberFromUser(message, defaultValue){
+    let moneyTemp = 0;
+    let defaultValueTemp = isNumber(defaultValue) ? defaultValue : '';
+    moneyTemp = prompt(message, defaultValueTemp);
+    if (!isValidValue(moneyTemp)){
+        alert(`Only numbers!!!\n` + message);//вывод доп сообщения для пользователя
+        moneyTemp = getNumberFromUser(message, defaultValue); //рекурсивно вызываем себя
     }
-    //нет простых чисел меньше двух
-    if(number < 2){
-        return false;
-    }
-    //если 2, то точно простое, выходим
-    if(number === 2){
-        return true;
-    }
-    //если число четное, то нет смысла в цикле
-    if (number%2 === 0){
-        return false;
-    }
-    //деление на 2 мы уже проверили, то есть надо начинать с деления на 3
-    //тогда точно нет смысла идти дальше трети числа,
-    //потому что всё, что больше, уже проверено
-    for(let i = 3; i < number/3+1; i+=2){
-        if(number%i === 0){
-            return false;
-        }
-    }
-    return true;
+    return moneyTemp;
 }
 
+/**
+ * Проверяет можно ли преобразовать переменную в число
+ * ВНИМАИЕ!!! Не меняет само число, а возвращает только bool
+ * @param number
+ * @returns {boolean|boolean}
+ */
+function isNumber(number) {
+    return !isNaN(parseFloat(number)) && isFinite(number);
+}
+
+/**
+ * Функция проверки ввода
+ * @param val - параметр - проверяемое число
+ * @returns {boolean} - true- число подходит, иначе нет
+ */
+function isValidValue(val) {
+    if(val === null){
+        return true;
+    }
+    if(isNumber(val)){
+        return true;
+    }
+    return  false;
+}
+
+/**
+ * Генерация случайного числа в диапазоне
+ * стырил (зато честно)))
+ * @param min - минимальное число диапазона
+ * @param max - максимальное число диапазона
+ * @returns {*}
+ */
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
